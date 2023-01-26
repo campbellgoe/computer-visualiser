b353899cdcc86032677968885d95dd99ea629409
George Campbell <campbell.goe@gmail.com>
2023-01-26 08:56:16 +0000

    fix build error

--- components/Three/Main/useComputerVisualiserApp.ts
+++ components/Three/Main/useComputerVisualiserApp.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import * as THREE from 'three';
 import { useEffect, useRef, useState } from "react"
 import initialiseScene from "./initialiseScene";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import ThreeApp from '../components/Three/ThreeApp'
 
 const IndexPage = () => {
